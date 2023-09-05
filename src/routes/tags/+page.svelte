03329c03ac37124c610d925e4f78207a2bf02714
hulk-higakijin <hm385.chejptks@gmail.com>
2023-09-06 02:22:19 +0900

    feat: /tags

---
+++ src/routes/tags/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Card } from 'flowbite-svelte';
+
+	export let data;
+</script>
+
+<div>
+	<h1>Tags</h1>
+
+	<div class="grid grid-cols-4 gap-2">
+		{#each data.tags as tag}
+			<Card href="/tags/{tag.id}">
+				<h2>{tag.name}</h2>
+			</Card>
+		{/each}
+	</div>
+</div>
